07febcfed1defbc76be6c18797cd4381f000bad8
thatisme <nicekunho@naver.com>
2024-09-12 17:02:15 +0900

    taewoo edited

--- src/main/java/com/lgdx/indiaCS/controller/process/ProcessViewController.java
+++ src/main/java/com/lgdx/indiaCS/controller/process/ProcessViewController.java
@@ -3,6 +3,7 @@ package com.lgdx.indiaCS.controller.process;
 import com.lgdx.indiaCS.domain.AsRequest;
 import com.lgdx.indiaCS.domain.Diagnose;
 import com.lgdx.indiaCS.domain.Repair;
+import com.lgdx.indiaCS.repository.AsRequestRepository;
 import com.lgdx.indiaCS.service.ASService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,61 +24,64 @@ public class ProcessViewController {
     @Autowired
     ASService asService;
 
+    @Autowired
+    AsRequestRepository asRequestRepository;
+
     @GetMapping("/process")
     public String reviewComplete(HttpSession session, Model model) {
 
+        System.out.println("프로세스 진입");
+        //사전세팅
+        model.addAttribute("date1", "");
+        model.addAttribute("date2", "");
+        model.addAttribute("date3", "");
+        model.addAttribute("date4", "");
+        model.addAttribute("date5", "");
+        model.addAttribute("date6", "");
+        model.addAttribute("diagnosisData", "");
+        model.addAttribute("repairData", "");
+
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-//        System.out.println("[입력받은 asRequestId in ProcessViewController]" + session.getAttribute("asRequestId"));
 
         // 세션에서 asRequestId 값을 가져옴
-        Object asRequestId = session.getAttribute("asRequestId");
+        String asRequestId = (String) session.getAttribute("asRequestId");
+        String userId = (String)session.getAttribute("userId");
 
-        // asRequestId가 null인지 확인
-        if (asRequestId != null) {
-            Optional<AsRequest>asRequestOptional = asService.asRequestInfo((String) asRequestId);
-            if(asRequestOptional.isPresent()){
-                AsRequest asRequest = asRequestOptional.get();
-                model.addAttribute("date1",formatter.format(asRequest.getAsRequestDate()));
-                Optional<Diagnose>diagnoseOptional = asService.diagnoseInfo(asRequest.getAsRequestId());
-                if(diagnoseOptional.isPresent()){
-                    Diagnose diagnose = diagnoseOptional.get();
-                    model.addAttribute("date2",formatter.format(diagnose.getDiagnoseDate()));
-                    model.addAttribute("diagnosisData",diagnose.getDiagnoseContent());
-                    List<Repair> repairList = asService.repairInfo(diagnose.getDiagnoseId());
-                    try{
-                        Repair repairState1 = repairList.get(0);
-                        model.addAttribute("date3",formatter.format(repairState1.getRepairDate()));
-                        model.addAttribute("repairData",repairState1.getRepairPartInventory());
-                    }catch(Exception e){
-                        model.addAttribute("date3","");
-                        model.addAttribute("RepairData","");
+        if (asRequestId == null && userId == null) {
+            return "redirect:/reservation-check";
+        }
+
+        if(userId != null) {
+            System.out.println("userId로 체크");
+            asRequestId = asRequestRepository.findByUserId(userId).get(0).getAsRequestId();
+            session.setAttribute("asRequestId" , asRequestId);
+            System.out.println("asRequestId = " + asRequestId );
+        }
+
+        AsRequest asRequest = asService.asRequestInfo(asRequestId);
+        if(asRequest != null) {
+            model.addAttribute("date1", formatter.format(asRequest.getAsRequestDate()));
+
+            Diagnose diagnose = asService.diagnoseInfo(asRequest.getAsRequestId());
+            if(diagnose != null) {
+                model.addAttribute("date2", formatter.format(diagnose.getDiagnoseDate()));
+                model.addAttribute("diagnosisData", diagnose.getDiagnoseContent());
+
+                List<Repair> repairList = asService.repairInfo(diagnose.getDiagnoseId());
+                int startIndex = 3;
+                for(Repair repair : repairList) {
+                    String key = "date" + startIndex;
+                    model.addAttribute(key , formatter.format(repair.getRepairDate()));
+
+                    if(startIndex == 3) {
+                        model.addAttribute("repairData", repair.getRepairPartInventory());
                     }
-                    try{
-                        Repair repairState2 = repairList.get(1);
-                        model.addAttribute("date4",formatter.format(repairState2.getRepairDate()));
-                    }catch(Exception e){
-                        model.addAttribute("date4","");
-                    }
-                    try{
-                        Repair repairState3 = repairList.get(2);
-                        model.addAttribute("date5",formatter.format(repairState3.getRepairDate()));
-                    }catch(Exception e){
-                        model.addAttribute("date5","");
-                    }
-                    try{
-                        Repair repairState4 = repairList.get(3);
-                        model.addAttribute("date6",formatter.format(repairState4.getRepairDate()));
-                    }catch(Exception e){
-                        model.addAttribute("date6","");
-                    }
-                }else {
-                    model.addAttribute("date2","");
-                    model.addAttribute("diagnosisData","");
+                    startIndex++;
                 }
-            }else {
-                model.addAttribute("date1","");
             }
         }
+
         return "/process";
     }
-    }
+
+}
